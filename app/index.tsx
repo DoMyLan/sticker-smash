072114592429c5b92f0ab21070adb74073a117f5
Do Thi My Lan <dtmlan@tma.com.vn>
2025-09-26 14:00:04 +0700

    add text in test_branch

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>eeeeee</Text>
     </View>
   );
 }
